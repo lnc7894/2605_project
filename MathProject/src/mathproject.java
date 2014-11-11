7cbf4621a14585d3e63692933b44e101abe6b4e1
lnc7894 <lauracrosby7894@gmail.com>
2014-11-11 15:30:09 -0500

    Started declaring a method
    I have yet to add in the matrix stuff from judy but it was a lot of
    files and I wasn't sure which ones we need to have.

--- MathProject/src/mathproject.java
+++ MathProject/src/mathproject.java
@@ -1,4 +1,15 @@
+import java.util.Vector;
+
 
 public class mathproject {
-    // we be doin this work yo
+    
+    //Vector<Float> v = new Vector<>();
+    public void power_method(float[][] matrix, float[] vector, float epsilon, int iterations) {
+        for (int i = 0; i <= iterations; i++) {
+            float[][] temp = matrix;
+            float[] vtemp = vector;
+            
+            //need to add in judy's files for matrix multiplication etc.
+        }
+    }
 }
