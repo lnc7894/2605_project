0c0705de8c41db911ddef7117b635995f953ec4a
duyt11 <duyt11@gmail.com>
2014-11-11 17:53:13 -0500

    Duy Update 002
    Jk we are gonna need the get method in Matrix. There's no matrix
    multiply method. Created one in the Matrix class but didn't finish.

--- MathProject/src/Matrix.java
+++ MathProject/src/Matrix.java
@@ -25,8 +25,22 @@ public class Matrix {
      * Get's the height of the matrix.
      * @return number of rows in matrix
      */
+    
     public int getHeight() {
         return matrix.length;
+    }
+    /**
+     * Gets value located at specified row and column
+     * @param i row
+     * @param j column
+     * @return double located at row i and column j in matrix
+     */
+    public float get(int i, int j) throws MatrixIndexOutOfBoundsException {
+        if (i >= width || j >= height) {
+            throw new MatrixIndexOutOfBoundsException("Index Out of Bounds");
+        } else {
+            return matrix[i][j];
+        }
     }
     /**
      * Gets the width of the matrix.
@@ -49,5 +63,22 @@ public class Matrix {
             aString = aString + "\n";
         }
         return aString;
+    }
+    /**
+     * Multiplies two matrices FOR POWER METHOD because it assumes that
+     * the matrices are both n x n.
+     * @param a
+     * @param b
+     * @return
+     */
+    public Matrix multiply(Matrix a, Matrix b) {
+        float[][] ma = new float[a.getHeight()][a.getWidth()];
+        for(int i = 0; i < a.getHeight(); i++) {
+            for (int j = 0; j < a.getWidth(); j++) {
+                //code needed but duy has to go to class
+            }
+        }
+        Matrix temp = new Matrix(ma);
+        return temp;
     }
 }
