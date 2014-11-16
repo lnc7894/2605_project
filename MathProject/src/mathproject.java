bacab89b42214148933b58e03bca54b3280921a2
duyt11 <duyt11@gmail.com>
2014-11-16 15:13:58 -0500

    YORO
    Gave up. Starting over. Fml

--- MathProject/src/mathproject.java
+++ MathProject/src/mathproject.java
@@ -1,20 +1,13 @@
-import java.util.Scanner;
-import java.util.Vector;
-
-
 public class mathproject {
-    
-    //Vector<Float> v = new Vector<>();
-    public void power_method(Matrix matrix, float[] vector, float epsilon, int iterations) {
-        for (int i = 0; i <= iterations; i++) {
-            Matrix temp = matrix.multiply(matrix, matrix);
-            float[] vtemp = vector;
-            //in order to multiply the matrix and the vector for the power method,
-            // we need judy's linearalgebra and vector files, so i added them
-            //but they are messed up and I'm not quite sure how to fix them.
-            
-            //need to add in judy's files for matrix multiplication etc.
-        }
+	/**
+	 * Power Method
+	 * @param matrix
+	 * @param vector
+	 * @param epsilon
+	 * @param iterations
+	 */
+    public void power_method(Matrix matrix, Vector vector, float epsilon, int iterations) {
+        //code
     }
     
     public void gn_qua() { //not void, just void for now
