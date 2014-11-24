f30c7b95a2098df3f079d294a9ae4204b54a8db2
lnc7894 <lauracrosby7894@gmail.com>
2014-11-24 17:49:20 -0500

    GN Stuff Continued

--- MathProject/src/Gn_Stuff.java
+++ MathProject/src/Gn_Stuff.java
@@ -13,7 +13,22 @@ public class Gn_Stuff {
         return value;
     }
     
-    public static double patel(int i, double x) {
+    public static double equationExp(double a, double b, double c, double x) {
+        double value = ((a*(Math.exp(b*x))) + c);
+        return value;
+    }
+    
+    public static double equationLog(double a, double b, double c, double x) {
+        double value = (a*(Math.log10(b + x)) + c);
+        return value;
+    }
+    
+    public static double equationRat(double a, double b, double c, double x) {
+        double value = (((a*x)/(x + b)) + c);
+        return value;
+    }
+    
+    public static double partialQua(int i, double x) {
         if (i ==  0) {
             return - Math.pow(x, 2);
         }
@@ -25,6 +40,48 @@ public class Gn_Stuff {
         }
         double fail = 0.0;
         return fail; // shouldn't get to this
+    }
+    
+    public static double partialExp(int i, double x, double b, double a) {
+        if (i == 0) {
+            return -Math.exp(b*x);
+        }
+        if (i == 1) {
+            return -(a*x*(Math.exp(b*x)));
+        }
+        if (i == 2) {
+            return -1;
+        }
+        double fail = 0.0;
+        return fail;
+    }
+    
+    public static double partialLog(int i, double x, double b, double a) {
+        if (i == 0) {
+            return -(Math.log10(b + x));
+        }
+        if (i == 1) {
+            return -(a/((Math.log(10))*(b + x)));
+        }
+        if (i == 2) {
+            return -1;
+        }
+        double fail = 0.0;
+        return fail;
+    }
+    
+    public static double partialRat(int i, double x, double b, double a) {
+        if (i == 0) {
+            return -(x/(x + b));
+        }
+        if (i == 1) {
+            return ((a*x)/((b + x)*(b + x)));
+        }
+        if (i == 2) {
+            return -1;
+        }
+        double fail = 0.0;
+        return fail;
     }
 
     public static void gn_qua() {
@@ -89,11 +146,15 @@ public class Gn_Stuff {
         RealMatrix Jacobi = new Array2DRowRealMatrix(j);
         
         for (int m = 0; m < Jacobi.getRowDimension(); m ++) {
-            Jacobi.setEntry(m, 0, patel(0, x[m]));
-            Jacobi.setEntry(m, 1, patel(1, x[m]));
-            Jacobi.setEntry(m, 2, patel(2, x[m]));
+            Jacobi.setEntry(m, 0, partialQua(0, x[m]));
+            Jacobi.setEntry(m, 1, partialQua(1, x[m]));
+            Jacobi.setEntry(m, 2, partialQua(2, x[m]));
         }
-        qr_fact_househ qrfact = qr_fact_househ(Jacobi);
+        //not finished
+        qr_fact_househ qrfact = new qr_fact_househ(Jacobi);
+        RealMatrix R = qrfact.getR();
+        RealMatrix Q = qrfact.getQ();
+        
         System.out.println(Jacobi);
     }
     
@@ -116,6 +177,26 @@ public class Gn_Stuff {
             e.printStackTrace();;
         }
         
+        String[] s1 = str.split(",");
+        double[] d1 = new double[s1.length];
+        int i = 0;
+        for (String s : s1) {
+            d1[i] = Double.parseDouble(s);
+            i++;
+        }
+        double[] x = new double[d1.length / 2];
+        double[] y = new double[d1.length / 2];
+        int k = 0;
+        for (int j = 0; j < x.length; j++) {
+            x[j] = d1[k];
+            k += 2;
+        }
+        k = 1;
+        for (int j1 = 0; j1 < y.length; j1++) {
+            y[j1] = d1[k];
+            k += 2;
+        }
+        
         System.out.println("Guess the parameter A");
         double a0 = scan.nextDouble();
 
@@ -129,6 +210,24 @@ public class Gn_Stuff {
 
         System.out.println("Guess the number of iterations");
         double numberOfIterations = scan.nextDouble();
+        
+        double[] residual = new double[x.length];
+        for (int n = 0; n < x.length; n++) {
+            residual[n] = y[n] - equationExp(a0, b0, c0, x[n]);
+        }
+        RealVector Res = new ArrayRealVector(residual);
+        double[][] j = new double[residual.length][3];
+        RealMatrix Jacobi = new Array2DRowRealMatrix(j);
+        
+        for (int m = 0; m < Jacobi.getRowDimension(); m ++) {
+            Jacobi.setEntry(m, 0, partialExp(0, x[m], b0, a0));
+            Jacobi.setEntry(m, 1, partialExp(1, x[m], b0, a0));
+            Jacobi.setEntry(m, 2, partialExp(2, x[m], b0, a0));
+        }
+        //nothing finished after this
+        qr_fact_househ qrfact = new qr_fact_househ(Jacobi);
+        RealMatrix R = qrfact.getR();
+        RealMatrix Q = qrfact.getQ();
     }
     
     public static void gn_log() {
@@ -150,6 +249,26 @@ public class Gn_Stuff {
             e.printStackTrace();;
         }
         
+        String[] s1 = str.split(",");
+        double[] d1 = new double[s1.length];
+        int i = 0;
+        for (String s : s1) {
+            d1[i] = Double.parseDouble(s);
+            i++;
+        }
+        double[] x = new double[d1.length / 2];
+        double[] y = new double[d1.length / 2];
+        int k = 0;
+        for (int j = 0; j < x.length; j++) {
+            x[j] = d1[k];
+            k += 2;
+        }
+        k = 1;
+        for (int j1 = 0; j1 < y.length; j1++) {
+            y[j1] = d1[k];
+            k += 2;
+        }
+        
         System.out.println("Guess the parameter A");
         double a0 = scan.nextDouble();
 
@@ -163,6 +282,24 @@ public class Gn_Stuff {
         
         System.out.println("Guess the number of iterations");
         double numberOfIterations = scan.nextDouble();
+        
+        double[] residual = new double[x.length];
+        for (int n = 0; n < x.length; n++) {
+            residual[n] = y[n] - equationLog(a0, b0, c0, x[n]);
+        }
+        RealVector Res = new ArrayRealVector(residual);
+        double[][] j = new double[residual.length][3];
+        RealMatrix Jacobi = new Array2DRowRealMatrix(j);
+        
+        for (int m = 0; m < Jacobi.getRowDimension(); m ++) {
+            Jacobi.setEntry(m, 0, partialLog(0, x[m], b0, a0));
+            Jacobi.setEntry(m, 1, partialLog(1, x[m], b0, a0));
+            Jacobi.setEntry(m, 2, partialLog(2, x[m], b0, a0));
+        }
+        //nothing finished after this
+        qr_fact_househ qrfact = new qr_fact_househ(Jacobi);
+        RealMatrix R = qrfact.getR();
+        RealMatrix Q = qrfact.getQ();
     }
     
     public static void gn_rat() {
@@ -184,6 +321,26 @@ public class Gn_Stuff {
             e.printStackTrace();;
         }
         
+        String[] s1 = str.split(",");
+        double[] d1 = new double[s1.length];
+        int i = 0;
+        for (String s : s1) {
+            d1[i] = Double.parseDouble(s);
+            i++;
+        }
+        double[] x = new double[d1.length / 2];
+        double[] y = new double[d1.length / 2];
+        int k = 0;
+        for (int j = 0; j < x.length; j++) {
+            x[j] = d1[k];
+            k += 2;
+        }
+        k = 1;
+        for (int j1 = 0; j1 < y.length; j1++) {
+            y[j1] = d1[k];
+            k += 2;
+        }
+        
         System.out.println("Guess the parameter A");
         double a0 = scan.nextDouble();
 
@@ -197,6 +354,24 @@ public class Gn_Stuff {
 
         System.out.println("Guess the number of iterations");
         double numberOfIterations = scan.nextDouble();
+        
+        double[] residual = new double[x.length];
+        for (int n = 0; n < x.length; n++) {
+            residual[n] = y[n] - equationRat(a0, b0, c0, x[n]);
+        }
+        RealVector Res = new ArrayRealVector(residual);
+        double[][] j = new double[residual.length][3];
+        RealMatrix Jacobi = new Array2DRowRealMatrix(j);
+        
+        for (int m = 0; m < Jacobi.getRowDimension(); m ++) {
+            Jacobi.setEntry(m, 0, partialRat(0, x[m], b0, a0));
+            Jacobi.setEntry(m, 1, partialRat(1, x[m], b0, a0));
+            Jacobi.setEntry(m, 2, partialRat(2, x[m], b0, a0));
+        }
+        //nothing finished after this
+        qr_fact_househ qrfact = new qr_fact_househ(Jacobi);
+        RealMatrix R = qrfact.getR();
+        RealMatrix Q = qrfact.getQ();
     }
     
     public static void main(String[] args) {
